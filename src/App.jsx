04ccd86d793dd9112650992067fe54a6f563e295
Flames <vibe-coder@flames.com>
2025-11-07 20:02:50 +0000

    Update project prj_jizmvaim - 2025-11-07T20:02:49.551Z

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import HeroSection from './components/HeroSection';
-import PainBlock from './components/PainBlock';
 import ValueBlock from './components/ValueBlock';
-import StepsBlock from './components/StepsBlock';
-import TargetBlock from './components/TargetBlock';
-import ImpactBlock from './components/ImpactBlock';
-import ReviewsBlock from './components/ReviewsBlock';
-import CTASection from './components/CTASection';
 import LeadForm from './components/LeadForm';
 import Footer from './components/Footer';
 
@@ -13,13 +7,7 @@ function App() {
   return (
     <div className="min-h-screen w-full bg-white text-gray-900">
       <HeroSection />
-      <PainBlock />
       <ValueBlock />
-      <StepsBlock />
-      <TargetBlock />
-      <ImpactBlock />
-      <ReviewsBlock />
-      <CTASection />
       <LeadForm />
       <Footer />
     </div>
